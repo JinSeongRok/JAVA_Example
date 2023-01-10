aba38de10b1b78b9c4691c8f5d4628391e0bc8f0
록 <121270701+JinSeongRok@users.noreply.github.com>
2023-01-10 19:11:12 +0900

    d
    d

--- java1205/src/java1206/boo1.java
+++ java1205/src/java1206/boo1.java
@@ -5,7 +5,7 @@ public class boo1 {
 	public static void main(String[] args) {
 		
 		String name = "홍길동"; //test
-		String job = "프로그래머";
+		String job = "프로그래머"; //test2
 		System.out.println(name);
 		System.out.println(job);
 		
